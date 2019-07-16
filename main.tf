5b787c7ecd3b6f34791fbc5b1be93b38c588a33e
marvinlandicho <marvinlandicho@mnlandicho-2.local>
2019-07-16 23:34:30 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,13 +48,20 @@ resource "aws_security_group" "zookeeper-server" {
     from_port   = 2181
     to_port     = 2181
     protocol    = "TCP"
-    cidr_blocks = []
+    cidr_blocks = ["10.0.0.0/16"]
   }
   
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 }
